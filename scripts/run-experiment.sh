df95af381383812922285375840cc68f08b8a6ae
Ugo Pattacini <ugo.pattacini@iit.it>
2020-07-26 16:09:53 +0200

    Update run-experiment.sh
    @vvasco this modification incorporates the target 7 as resulting from your analysis done in #10

--- scripts/run-experiment.sh
+++ scripts/run-experiment.sh
@@ -12,7 +12,7 @@ yarp wait /icubSim/right_arm/state:o
 yarpmanager-console --application experiment.xml --run --connect --exit --silent
 yarp wait /actionsRenderingEngine/cmd:io
 echo "home all" | yarp rpc /actionsRenderingEngine/cmd:io
-echo "grasp (cartesian -0.45 0.0 0.0 0.0 0.0 1.0 3.14159) left still" | yarp rpc /actionsRenderingEngine/cmd:io
+echo "grasp (cartesian -0.35 0.0 0.0 0.0 0.0 1.0 3.14159) left still" | yarp rpc /actionsRenderingEngine/cmd:io
 
 declare -a modules=("actionsRenderingEngine" "wholeBodyDynamics" "iKinGazeCtrl" "iKinCartesianSolver" "yarprobotinterface" "gzclient" "gzserver" "yarpserver")
 for module in ${modules[@]}; do
