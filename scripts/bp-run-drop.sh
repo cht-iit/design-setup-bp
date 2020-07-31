78e244b03f3e76ea09f18c83b5d8c14beb70b4c7
Ugo Pattacini <ugo.pattacini@iit.it>
2020-07-31 08:54:23 +0000

    fixed loading of XML applications
    cc @vvasco

--- scripts/bp-run-drop.sh
+++ scripts/bp-run-drop.sh
@@ -9,7 +9,7 @@ yarp wait /icubSim/head/state:o
 yarp wait /icubSim/left_arm/state:o
 yarp wait /icubSim/right_arm/state:o
 
-yarpmanager-console --application bp-experiment.xml --run --connect --exit --silent
+yarpmanager-console --application ${ICUBcontrib_DIR}/share/ICUBcontrib/applications/bp-experiment.xml --run --connect --exit --silent
 yarp wait /actionsRenderingEngine/cmd:io
 echo "home all" | yarp rpc /actionsRenderingEngine/cmd:io
 echo "grasp (cartesian -0.3 -0.3 -0.05 0.0 0.0 1.0 3.49066) (approach (0.0 0.0 0.07 0.0)) left still" | yarp rpc /actionsRenderingEngine/cmd:io
